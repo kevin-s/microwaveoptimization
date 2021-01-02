006b8beb0dedc315068b02b2a27ba1d0de929aaf
Kevin Shannon <kevinshannon@kevins-mbp-59b8.lan>
2021-01-01 21:51:51 -0600

    adding initial files and first method

---
+++ microwaveoptimization/main.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    print("Hello World!")
+
+
+
+
+
+
+def thirtySecondIncrements(seconds):
+    if seconds % 30 != 0:
+        return None
+    return seconds / 30
---
+++ microwaveoptimization/test_main.py
@@ -0,0 +1,20 @@
+import unittest
+import main
+
+
+class MyTestCase(unittest.TestCase):
+    def test_thirtySecondIncrements_givenThirty_shouldReturnOne(self):
+        actual = main.thirtySecondIncrements(30)
+        self.assertEqual(1, actual)
+
+    def test_thirtySecondIncrements_givenTwentyFive_shouldReturnNone(self):
+        actual = main.thirtySecondIncrements(25)
+        self.assertEqual(None, actual)
+
+    def test_thirtySecondIncrements_givenSixty_shouldReturnTwo(self):
+        actual = main.thirtySecondIncrements(60)
+        self.assertEqual(2, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
